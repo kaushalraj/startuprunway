5fae95c3e3d7b52d8bbd66eea5f12ea0efeccfdc
Rajendra Prasad Palnaty <rppalnaty@gmail.com>
2025-10-03 18:36:29 +0000

    AI ChatBot and CTA buttons are overlapping, added z-index to CTA button

--- app/page.tsx
+++ app/page.tsx
@@ -257,7 +257,7 @@ export default function StartupRunwayLanding() {
 	      One-Stop Startup Hub
 	      </h1>
 	      <h2 className="text-xl sm:text-2xl md:text-2xl lg:text-3xl font-semibold mb-6 text-white leading-snug">
-	      SaaS Enabled and Services for Every Stage of Your Startup 
+	      Empowering Indian Startups to Launch, Scale, and Succeed 
 	      </h2>
               <p className="text-sm sm:text-base md:text-lg lg:text-xl text-slate-200 mb-6 leading-relaxed" > 
 	      Founders | Entreprenuers | Studentprenuers | Service Partners | Investors
