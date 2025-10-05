13d51a9383aff885fa9b6cb70c3cb9a1ac11ab89
Rajendra Prasad Palnaty <rppalnaty@gmail.com>
2025-10-05 20:08:06 +0000

    One Platform to Grow to Bold

--- app/page.tsx
+++ app/page.tsx
@@ -256,13 +256,13 @@ export default function StartupRunwayLanding() {
               <h1 className="text-3xl md:text-5xl font-bold mb-6 text-white leading-tight">
 	      One-Stop Startup Hub
 	      </h1>
-	      <p className="text-xl font-bold md:text-2xl text-slate-300 mb-8 leading-relaxed">
+	      <p className="text-xl md:text-2xl text-white text-slate-300 mb-8 leading-relaxed">
 	      Empowering Indian Startups to Launch, Scale, and Succeed 
 	      </p>
-              <p className="text-base font-bold md:text-lg text-slate-400 mb-8 leading-relaxed" >
+              <p className="text-base md:text-lg text-white text-slate-400 mb-8 leading-relaxed" >
 	      Founders | Entreprenuers | Studentprenuers | Service Partners | Investors
 	      </p>
-	      <p className="text-lg sm:text-lg font-bold md:text-xl lg:text-2xl text-slate-300 mb-8 leading-relaxed">
+	      <p className="text-lg text-white sm:text-lg md:text-xl lg:text-2xl text-slate-300 mb-8 leading-relaxed">
 	      One Platform to Grow 
 	      </p>
               <p className="text-base md:text-lg text-slate-300 mb-8 leading-relaxed">
