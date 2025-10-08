b9ae2c55952b6c63e8aaf45ee9433e20a49d28b9
Rajendra Prasad Palnaty <rppalnaty@gmail.com>
2025-10-08 23:54:06 +0000

    Page Adding to Studentpreneur page

--- app/studentpreneur/page.tsx
+++ app/studentpreneur/page.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 function Header() {
   return (
     <header className="fixed top-0 left-0 w-full flex items-center gap-3 px-6 py-4 bg-[#0f1233]/90 backdrop-blur-md z-50">
-	<Link href="/" className="flex flex-col items-center">
+	<Link href="/" className="flex items-center gap-3 cursor-pointer">
       <Image src="/images/startuprunway-logo.png" alt="StartupRunway Logo" width={50} height={50} />
       <h1 className="text-2xl md:text-3xl font-bold text-white">StartupRunway</h1>
 	</Link>
