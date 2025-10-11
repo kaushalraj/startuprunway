8c9f2aea5da273748c0009edb78419036e8c2e94
Rajendra Prasad Palnaty <rppalnaty@gmail.com>
2025-10-11 18:27:02 +0000

    service partners page added

--- app/service-partners/page.tsx
+++ app/service-partners/page.tsx
@@ -108,7 +108,7 @@ function HeroSection() {
 
         <Button className="bg-[#ff6b35] hover:bg-[#ff6b35]/90 text-white px-8 py-6 rounded-full group">
           <Rocket className="w-5 h-5 mr-2 group-hover:translate-y-[-2px] transition-transform" />
-          Join as Partner
+          Join as Service Partner
         </Button>
       </div>
     </section>
