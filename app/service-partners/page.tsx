ba35bb1fc9711a06bef9906c8357616c1d9fc2ba
Rajendra Prasad Palnaty <rppalnaty@gmail.com>
2025-10-11 07:39:38 +0000

    service partners page added

--- app/service-partners/page.tsx
+++ app/service-partners/page.tsx
@@ -169,7 +169,7 @@ function PartnerCategories() {
   ];
 
   return (
-    <section className="py-20 px-4">
+    <section className="py-10 px-4">
       <div className="max-w-6xl mx-auto">
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
